bc3b5d1f76cf8cb9421c569ee8eb25c465e21109
Gowtham Raghunathan <49944971+GowthamR12@users.noreply.github.com>
2021-08-15 15:22:00 +0530

    Update stud_choose.php

--- p/stud_choose.php
+++ p/stud_choose.php
@@ -10,6 +10,7 @@ session_start();
 <a href="stud_video.php">VIDEOS</html><br>
 <a href="stud_assign.php">ASSIGNMENT</html><br>
 <a href="stud_quiz.php">QUIZ</html><br>
+<a href="http://localhost:9966">VIDEO CONFERENCE</a>
 <br>
  <a href="student_home.php">GO HOME</a>
 <a href="logout.php">LOGOUT</a>
